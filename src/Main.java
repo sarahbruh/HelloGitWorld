1c28d34ba9c75898df0bff094dc28f87323f67e1
Sarah <brunmayr14mail.com>
2020-10-14 19:56:10 +0200

    Differenz

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        boolean t = true;
+
+        while(t){
+
+            try{
+                System.out.printf("Bitte geben Sie einen Wert ein: %n");
+                int a = sc.nextInt();
+
+                System.out.printf("Bitte geben Sie einen Wert ein: %n");
+                int b = sc.nextInt();
+
+                int summe = a - b;
+
+                System.out.println("Die Summe beträgt " +summe);
+                System.out.println("----------------------------------------------");
+            }
+            catch (InputMismatchException ex){
+
+                System.out.println("Fehler!");
+            }
+        }
     }
 }
